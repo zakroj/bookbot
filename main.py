9a82077b4dec03a1ee38b10770664773fc2f187c
zakroj <zakroj@hotmail.com>
2024-02-02 10:03:03 -0500

    added function that counts number of occurance of each alphabet letter in the text

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    print(get_num_characters(text))
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,3 +12,13 @@ def get_book_text(path):
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_num_characters(text):
+    char_dict = {}
+    full_text = text.lower()
+    for c in "abcdefghijklmnopqrstuvyxyz":
+        count = full_text.count(c)
+        char_dict[c] = count
+    return char_dict
+
+main()
