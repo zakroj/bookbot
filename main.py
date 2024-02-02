62eb39b531066a0ff8bc261d0c6a7d6c738ccbd7
zakroj <zakroj@hotmail.com>
2024-02-02 00:45:57 -0500

    refactored main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
-print(file_contents)
+split_words = file_contents.split()
+print(len(split_words))
